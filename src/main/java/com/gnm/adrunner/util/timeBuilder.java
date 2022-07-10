0e1dea483ede6157ea340ec5643b50c80679f01d
jasonyeong790 <jasonyeong790@pood.pet>
2022-07-10 20:54:37 +0900

    시간 유틸리티 수정

--- src/main/java/com/gnm/adrunner/util/timeBuilder.java
+++ src/main/java/com/gnm/adrunner/util/timeBuilder.java
@@ -4,69 +4,100 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-
+ 
+import java.util.Locale;
+import java.util.TimeZone;
+    
 public class timeBuilder {
-
-    // 현재 시각 반환
-    public static String getCurrentTime(){
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
-    }
-
-
-    // 현재 시각 반환
-    public static Date toDate(String time) throws ParseException{
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(time);
-    }
-
     
-    // 어제 날짜로 00:00:00 시각 반환
-    public static String getYesterdayStartTime(){
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DATE, -1);
-        return new SimpleDateFormat("yyyy-MM-dd 00:00:00").format(c.getTime());
-    }
-
-    // 어제 날짜로 23:59:59 시각 반환
-    public static String getYesterdayEndTime(){
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DATE, -1);
-        return new SimpleDateFormat("yyyy-MM-dd 23:59:59").format(c.getTime());
-    }
-
-    // 어제 날짜 반환
-    public static String getYesterdayDate(){
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DATE, -1);
-        return new SimpleDateFormat("yyyyMMdd").format(c.getTime());
-    }
-
-    // 오늘 날짜 반환
-    public static String getTodayDate(){
-        Calendar c = Calendar.getInstance();
-        return new SimpleDateFormat("yyyyMMdd").format(c.getTime());
-    }
-
-    // 오늘 날짜로 00:00:00 시각 반환
-    public static String getTodayStartTime(){
-        return new SimpleDateFormat("yyyy-MM-dd 00:00:00").format(Calendar.getInstance().getTime());
-    }
-
-    // 오늘 날짜로 23:59:59 시각 반환
-    public static String getTodayEndTime(){
-        return new SimpleDateFormat("yyyy-MM-dd 23:59:59").format(Calendar.getInstance().getTime());
-    }
-
-
-    // 유효한 시간 타입인지 확인
-    public static Boolean isValidDateTime(String inDate){
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        dateFormat.setLenient(false);
-        try {
-            dateFormat.parse(inDate.trim());
-        } catch (ParseException pe) {
-            return false;
+    
+        public static TimeZone          timeZone            = TimeZone.getTimeZone("Asia/Seoul");
+    
+        public static SimpleDateFormat  simpleDateFormat    = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+    
+        public static SimpleDateFormat  simpleDateFormat1   = new SimpleDateFormat("yyyy-MM-dd 00:00:00", Locale.KOREA);
+    
+        public static SimpleDateFormat  simpleDateFormat2   = new SimpleDateFormat("yyyy-MM-dd 23:59:59", Locale.KOREA);
+    
+        public static SimpleDateFormat  simpleDateFormat3   = new SimpleDateFormat("yyyyMMdd", Locale.KOREA);
+    
+        // 현재 시각 반환
+        public static String getCurrentTime(){
+            simpleDateFormat.setTimeZone(timeZone);
+            return simpleDateFormat.format(Calendar.getInstance(timeZone).getTime());
         }
-        return true;
-    }
     
+        // 현재 시각 반환
+        public static Date toDate(String time) throws ParseException{
+            simpleDateFormat.setTimeZone(timeZone);
+            return simpleDateFormat.parse(time);
+        }
+    
+        
+        // 어제 날짜로 00:00:00 시각 반환
+        public static String getYesterdayStartTime(){
+            Calendar c = Calendar.getInstance(timeZone);
+            c.add(Calendar.DATE, -1);
+            return simpleDateFormat1.format(Calendar.getInstance(timeZone).getTime());
+        }
+    
+    
+        // 어제 날짜로 23:59:59 시각 반환
+        public static String getYesterdayEndTime(){
+            Calendar c = Calendar.getInstance(timeZone);
+            c.add(Calendar.DATE, -1);
+            return simpleDateFormat2.format(Calendar.getInstance(timeZone).getTime());
+        }
+        
+    
+        // 어제 날짜 반환
+        public static String getYesterdayDate(){
+            Calendar c = Calendar.getInstance(timeZone);
+            c.add(Calendar.DATE, -1);
+            return simpleDateFormat3.format(Calendar.getInstance(timeZone).getTime());
+        }
+    
+    
+    
+        // 전 날짜 반환 : day 만큼의 전 날짜 반환
+        public static String getPreviousDate(Integer day){
+            Calendar c = Calendar.getInstance(timeZone);
+            c.add(Calendar.DATE, day);
+            return simpleDateFormat3.format(Calendar.getInstance(timeZone).getTime());
+        }
+    
+    
+    
+    
+        // 오늘 날짜 반환
+        public static String getTodayDate(){
+            return simpleDateFormat3.format(Calendar.getInstance(timeZone).getTime());
+        }
+    
+    
+        // 오늘 날짜로 00:00:00 시각 반환
+        public static String getTodayStartTime(){
+            return simpleDateFormat1.format(Calendar.getInstance(timeZone).getTime());
+        }
+    
+    
+        // 오늘 날짜로 23:59:59 시각 반환
+        public static String getTodayEndTime(){
+            return simpleDateFormat2.format(Calendar.getInstance(timeZone).getTime());
+        }
+    
+        
+        // 유효한 시간 타입인지 확인
+        public static Boolean isValidDateTime(String inDate){
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            dateFormat.setLenient(false);
+            try {
+                dateFormat.parse(inDate.trim());
+            } catch (ParseException pe) {
+                return false;
+            }
+            return true;
+        }
+        
 }
+    
